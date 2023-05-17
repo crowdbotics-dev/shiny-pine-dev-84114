23dec26be4c03fedacf9b9707eda05dc2852f47f
Crowdbotics <engineering@crowdbotics.com>
2023-05-17 23:16:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-pine-dev-84114",
+  url: "https://shiny-pine-dev-84114.botics.co",
+  api: "https://shiny-pine-dev-84114.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
